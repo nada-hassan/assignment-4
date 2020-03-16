741552f5eb990fe288f0b19092a7f71834839771
nada-hassan <60989613+nada-hassan@users.noreply.github.com>
2020-03-16 06:41:46 +0200

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,20 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs76_cs88;
+
+public class Node {
+	/**
+	 *
+	 */
+	public Object element;
+	/**
+	 *
+	 */
+	public Node next = null;
+
+	/**
+	 * @param value
+	 */
+	public Node(Object value) {
+			this.element = value;
+	}
+}
+
